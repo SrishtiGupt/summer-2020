de2ba589d1711cf423ad09dce30c719356d7d071
SrishtiGupt <55215634+SrishtiGupt@users.noreply.github.com>
2020-07-28 08:44:22 -0700

    Update plot_day_or_night.py

--- plot_day_or_night.py
+++ plot_day_or_night.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 from os import path
 import datetime
 from matplotlib.dates import date2num, MINUTES_PER_DAY, SEC_PER_DAY
@@ -11,10 +12,13 @@ from datetime import datetime, timedelta
 
 subjectid = input("Subject ID: ")
 fname1 = input("Date: ")
-day_or_night = input("day or night: ")
+day_or_night = input("Day or Night: ")
+next_day = datetime.strptime(fname1, "%Y-%m-%d")
+modified_date = next_day + timedelta(days=1)
+fname2 = datetime.strftime(modified_date, "%Y-%m-%d")#month+"-"+next_day+"-"+year
 #read and clean a file
-def rcf(type,fname, day_or_night):
-    filepath1 = "/home/agmfamily/Tutorial/"+subjectid+"/"+fname+"_"+type+"_"+day_or_night+".csv"
+def rcf(subjectid,type,fname,day_or_night):
+    filepath1 = os.getcwd()+"/"+subjectid+"/"+fname+"_"+type+"_"+day_or_night+".csv"
     if path.exists(filepath1):
         df = pd.read_csv(filepath1)
         df.drop_duplicates(inplace = True)
@@ -23,41 +27,26 @@ def rcf(type,fname, day_or_night):
         print(fname, " does not exist")
         exit()
     return df
-titles = [fname1+" BVP", fname1+" EDA", fname1+" TEMP"]
-fig, ax1 = plt.subplots(3, sharex = True)
+
 i = 0
+titles = [fname1+" BVP",fname1+" EDA",fname1+" TEMP"]
+fig, ax1 = plt.subplots(3, sharex = True)
 for item in ['BVP', 'EDA', 'TEMP']:
-    df = rcf(item, fname1, day_or_night)
+    df = rcf(subjectid,item, fname1,day_or_night)
     fnum2 = np.array(df.iloc[:,1]).astype(float)
     fnum1 = np.array(df["UTCTimeStamp"]).astype(float)
-    if fnum1[-1]-fnum1[0]>3600:
-        time = []
-        data = []
-#converting UTC time into HH/MM/SS
-        for idx in range(len(fnum1)):
-            d = datetime.utcfromtimestamp(fnum1[idx])
-            if day_or_night == "day":
-                if d.hour<20 and d.hour>=8:
-                    time.append(d)
-                    data.append(fnum2[idx])
-            #    ax1[i].plot(time, data)
-            #    plt.xlabel("Time")
-            #    ax1[i].set_title(titles[i]+"_"+day_or_night)
-            #    ax1[i].xaxis.set_major_formatter(DateFormatter('%d:%H:%M:%S'))
-            #    plt.setp(ax1[i].get_xticklabels(), rotation=30, ha="right")
-            elif day_or_night == "night":
-                if d.hour>=20 and d.hour<8:
-                    time.append(d)
-                    data.append(fnum2[idx])
-        ax1[i].plot(time, data)
-        plt.xlabel("Time")
-        ax1[i].set_title(titles[i]+"_"+day_or_night)
-        ax1[i].xaxis.set_major_formatter(DateFormatter('%d:%H:%M:%S'))
-        plt.setp(ax1[i].get_xticklabels(), rotation=30, ha="right")
-    else:
-        print ("ERROR: more data expected for ", item)
-        break
-    i = i+1
-    print(i)
+    time = []
+    data = []
+    for idx in range(len(fnum1)):
+        d= datetime.utcfromtimestamp(fnum1[idx])
+        time.append(d)
+        data.append(fnum2[idx])
+    ax1[i].plot(time,data)
+    plt.xlabel("Time")
+    ax1[i].set_title(titles[i]+" "+day_or_night)
+    ax1[i].xaxis.set_major_formatter(DateFormatter('%d:%H:%M:%S'))
+    plt.setp(ax1[i].get_xticklabels(), rotation=30, ha="right")
+    print (item + " plotted")
+    i=i+1
 plt.suptitle("Subject "+ subjectid)
 plt.show()
